f5f9d736193d00c3bbff5016ea06a34c85507c66
yu641 <blindthewind@gmail.com>
2024-11-25 02:43:34 +0900

    Update st2.py

--- st2.py
+++ st2.py
@@ -2,8 +2,4 @@ import streamlit as st
 import pyogrio
 
 st.title('시군구별 합계출산율')
-
-from PIL import Image
-
-img=Image.open("C:/Users/yu/Figure_1.png")
-st.image(img)
+st.image("C:/Users/yu/Figure_1.png")
