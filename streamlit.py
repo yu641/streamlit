ed95fb5ca75879605614758757d2ae5b05f54596
yu641 <blindthewind@gmail.com>
2024-11-24 22:27:07 +0900

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+
+st.title('시군구별 합계출산율')
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] ='Malgun Gothic'
+
+gdf_korea_sido = gpd.read_file('C:/vis/geodata/sido.json')
+
+
+df=pd.read_excel("C:/vis/totalfertilityratenew.xlsx")
+
+
+korea_5179 = gdf_korea_sido.to_crs(epsg=5179,inplace=False)
+
+
+gp=df[['합계출산율']]
+print(gp.head())
+
+gdf=pd.concat([gdf_korea_sido, gp], axis=1)
+print(gdf)
+
+ax = gdf.plot(column='합계출산율', legend=True, cmap="Reds", k=5)
+ax.set_axis_off()
+ax.set_title("시군구별 합계출산율")
+plt.show()
